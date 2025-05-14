254c066c600d0dec698ff7434f7def401780b150
Vasyl Petsko <petsko.vi@gmail.com>
2025-05-14 08:33:53 +0300

    add reducers

--- src/redux/actionCreators/usersActionCreators.ts
+++ src/redux/actionCreators/usersActionCreators.ts
@@ -6,3 +6,31 @@ export const setUsersList = ( usersList: object []) => {
     payload: usersList
   }
 }
+
+export const deleteUser = (id: number) => {
+  return {
+    type: USERS.DELETE_USER,
+    payload: {
+      id
+    }
+  }
+}
+
+export const updateUser = (id: number, user: object) => {
+  return {
+    type: USERS.UPDATE_USER,
+    payload: {
+      id,
+      user
+    }
+  }
+}
+
+export const createUser = (user: object) => {
+  return {
+    type: USERS.CREATE_USER,
+    payload: {
+      user
+    }
+  }
+}
--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -19,6 +19,38 @@ export default (state: IUsersState  = initialState, action) => {
       return {
         ...state, usersList: [...action.payload]
       }
+    case USERS.CREATE_USER:
+    {
+      const id = new Date().getTime();
+      return {
+        ...state, usersList: [...state.usersList, {id, ...action.payload.user}]
+      };
+    }
+    case USERS.DELETE_USER:
+    {
+      const usersList = state.usersList.filter((user: object) => {
+        if (user.id === action.payload.id) {
+          return false;
+        }
+        return true;
+      });
+      return {
+        ...state, usersList: [...usersList]
+      };
+    }
+    case USERS.UPDATE_USER:
+    {
+      const usersList = state.usersList.map((user: object) => {
+        if (user.id === action.payload.id) {
+          user = action.payload.user;
+        }
+        return user;
+      });
+      return {
+        ...state, usersList: [...usersList]
+      };
+    }
+
     default:
       return state
   }
